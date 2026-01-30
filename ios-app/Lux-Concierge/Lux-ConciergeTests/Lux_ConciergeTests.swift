9c0dc30b8445fd82b802dec5276847f70c2a509e
Sophia Yan <sophiayan214@gmail.com>
2026-01-30 10:30:23 -0500

    Initial project setup: iOS app + backend scaffold

---
+++ ios-app/Lux-Concierge/Lux-ConciergeTests/Lux_ConciergeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Lux_ConciergeTests.swift
+//  Lux-ConciergeTests
+//
+//  Created by sophia on 1/30/26.
+//
+
+import Testing
+@testable import Lux_Concierge
+
+struct Lux_ConciergeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
